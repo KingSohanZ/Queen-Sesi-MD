8fe14ef8fd83225e822db986cf867e48487a08c6
SohanX_Mathiya <130471209+KingSohanZ@users.noreply.github.com>
2024-08-22 12:22:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "7Swh3IAI#EsTnz6HywZmwx4GCR9K-zMSe9eB1p65uOyU3cPxEIls",
+SESSION_ID: process.env.SESSION_ID || "7Swh3IAI#EsTnz6HywZmwx4GCR9K-zMSe9eB1p65uOyU3cPxEIls",
+ALIVE_IMG: process.env.ALIVE_IMG || "",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Queen Nishu And My Developer King Is ║Sohan Matheesha...😌💗⚒️",
 };
