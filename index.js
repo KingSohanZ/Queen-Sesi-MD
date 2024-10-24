0ec6e15ea0ae92ee4d482ca39c402c45485747f8
SohanX_Mathiya <130471209+KingSohanZ@users.noreply.github.com>
2024-10-24 14:41:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94725114153']
+const ownerNumber = ['94757962326']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
